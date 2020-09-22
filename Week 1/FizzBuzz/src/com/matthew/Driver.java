f4fb976d92397a7e1e5e28f8b13d9b74c50618a9
mtwhite4 <47282084+mtwhite4@users.noreply.github.com>
2020-09-22 10:51:40 -0400

    FizzBuzz Revised

--- Week 1/FizzBuzz/src/com/matthew/Driver.java
+++ Week 1/FizzBuzz/src/com/matthew/Driver.java
@@ -22,16 +22,17 @@ public class Driver {
 		
 		for (int i = 1; i <= a; i++) {
 			
-			if (i%3 == 0 && i%5 == 0 ) {
-				System.out.println("FizzBuzz");
-			} else if (i%3 == 0) {
-				System.out.println("Fizz");
-				
-			} else if (i%5 == 0) {
-				System.out.println("Buzz");
-			} else {
-				System.out.println(i);
+
+			if (i%3 == 0) {
+				System.out.print("Fizz");	
+			} 
+			if (i%5 == 0) {
+				System.out.print("Buzz");
+			} 
+			if (i%3 != 0 && i%5 != 0){
+				System.out.print(i);
 			}
+			System.out.println();
 		}
 	}
 }
