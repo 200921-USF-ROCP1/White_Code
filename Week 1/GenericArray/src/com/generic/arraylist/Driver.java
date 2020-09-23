f5657c3e99202e452ba9fc3a7f170dc09586e545
mtwhite4 <47282084+mtwhite4@users.noreply.github.com>
2020-09-23 14:15:08 -0400

    Updated Array List

--- Week 1/GenericArray/src/com/generic/arraylist/Driver.java
+++ Week 1/GenericArray/src/com/generic/arraylist/Driver.java
@@ -7,15 +7,14 @@ public class Driver {
 	public static void main(String[] args) {
 		
 		ArrayList<Integer> arr = new ArrayList(1);
-		Integer[][] sarr;
 		
 		Integer[] entries = {5,6,7,8,9,10,22,45,56,1,2};
 		arr.add(entries);
 		
 		
-		System.out.println(Arrays.deepToString(arr.get())); //original arra
+		System.out.println(Arrays.deepToString(arr.get())); //original array
 
-		System.out.println(Arrays.deepToString(arr.split(3)));
+		System.out.println(Arrays.deepToString(arr.split(4))); //split array
 		
 	}
 	
